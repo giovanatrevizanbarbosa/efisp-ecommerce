f8f772f4e652518fdee202c8b798f2f990657e92
giovanatrevizanbarbosa <gi.trevizan.barbosa@gmail.com>
2024-07-31 15:46:12 -0300

    add startup files

---
+++ src/test/java/TestHello.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestHello {
+    @Test
+    public void testHello(){
+        String expected = "Hello World!";
+        String actual = "Hello World!";
+
+        assertEquals(expected, actual);
+    }
+}
