7a7fab28eed07621287e75eb34b069197388f7c2
igorcardosoy <igorfilipicardoso@gmail.com>
2024-08-21 21:35:21 -0300

    fix: remove userRec

--- src/main/java/efisp/efispecommerce/models/dao/UserRec.java
+++
@@ -1,9 +0,0 @@
-package efisp.efispecommerce.models.dao;
-
-public record UserRec(long id, String name, String email, String password) implements Writable {
-
-    @Override
-    public String[] toCSV() {
-        return new String[]{String.valueOf(id), name, email, password};
-    }
-}
